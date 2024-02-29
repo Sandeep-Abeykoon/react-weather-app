739f6b800fedee3052536ccaca7c39c3f47268bc
Sandeep Abeykoon <97498102+Sandeep-Abeykoon@users.noreply.github.com>
2024-02-29 11:28:19 +0530

    updated the code

--- src/Components/TemperatureAndDetails/TemperatureAndDetails.jsx
+++ src/Components/TemperatureAndDetails/TemperatureAndDetails.jsx
@@ -11,12 +11,13 @@ const TemperatureAndDetails = () => {
       <div className={styles.details}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Simple_orange_circle.svg/2048px-Simple_orange_circle.svg.png" alt="" />
         <p>34°</p>
-        <div className={styles.stats}>
+        <div className={styles.basicStats}>
             <div className={styles.stat}><UilTemperature/><span>Real Feel : </span><span style={{fontWeight:"bold"}}>38°</span></div>
             <div className={styles.stat}><UilTear/><span>Humidity : </span><span style={{fontWeight:"bold"}}>65%</span></div>
-            <div className={styles.stat}><UilWind/><span>Wind Speed :</span><span style={{fontWeight:"bold"}}>11Kmph</span></div>
+            <div className={styles.stat}><UilWind/><span>Wind :</span><span style={{fontWeight:"bold"}}>11Kmph</span></div>
         </div>
       </div>
+      <div className={styles.highLowStats}>hey</div>
       </div>
 
   )
