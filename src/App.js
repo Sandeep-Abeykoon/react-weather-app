ec38cfd1fc3c52048f930bbf4614ef9b70bcde62
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-01 16:16:57 +0530

    updated the code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { TopButtons } from "./Components/TopButtons/TopButtons"
-import './App.css';
-import { Inputs } from './Components/Inputs/Inputs';
-import { TimeAndLocation } from './Components/TimeAndLocation/TimeAndLocation';
-import TemperatureAndDetails from './Components/TemperatureAndDetails/TemperatureAndDetails';
+import { TopButtons } from "./Components/TopButtons/TopButtons";
+import "./App.css";
+import { Inputs } from "./Components/Inputs/Inputs";
+import { TimeAndLocation } from "./Components/TimeAndLocation/TimeAndLocation";
+import TemperatureAndDetails from "./Components/TemperatureAndDetails/TemperatureAndDetails";
 import { Forecast } from "./Components/Forecast/Forecast";
+import getWeatherData from "./Services/WeatherService";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", {q:'london'});
+    console.log(data);
+  };
+
+  fetchWeather();
   return (
-  <div className='App'>
-    <TopButtons/>
-    <Inputs/>
-    <TimeAndLocation/>
-    <TemperatureAndDetails/>
-    <Forecast title="Hourly"/>
-    <Forecast title="Daily"/>
-  </div>
+    <div className="App">
+      <TopButtons />
+      <Inputs />
+      <TimeAndLocation />
+      <TemperatureAndDetails />
+      <Forecast title="Hourly" />
+      <Forecast title="Daily" />
+    </div>
   );
 }
 
