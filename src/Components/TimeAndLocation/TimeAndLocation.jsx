0f49884c23f3679519186610f564137cb3060a10
Sandeep-Abeykoon <sandeepchanura@gmail.com>
2024-03-04 15:01:19 +0530

    updated the code

--- src/Components/TimeAndLocation/TimeAndLocation.jsx
+++ src/Components/TimeAndLocation/TimeAndLocation.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./TimeAndLocation.module.css";
 
 export const TimeAndLocation = ({
   weather: { localTime, localDate, name, country },
 }) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.timeAndLocation}>
       <span className={styles.dateTime}>
-        {`${localDate} | Local time: ${localTime}`}
+        <span>{localDate}</span>
+        {isLargeScreen && <span>{"\u00A0|\u00A0"}</span>}
+        <span className={styles.dateTime}>Local time: {localTime}</span>
       </span>
       <div className={styles.city}>{`${name}, ${country}`}</div>
     </div>
